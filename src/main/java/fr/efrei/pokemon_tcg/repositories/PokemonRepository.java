6761a1be830889877894751d214668b906ce266f
samuel-poinama <samuel.poinama@efrei.net>
2025-01-29 16:36:28 +0100

    get all pokemons (by page)

---
+++ src/main/java/fr/efrei/pokemon_tcg/repositories/PokemonRepository.java
@@ -0,0 +1,12 @@
+package fr.efrei.pokemon_tcg.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.efrei.pokemon_tcg.models.Pokemon;
+
+
+@Repository
+public interface PokemonRepository extends JpaRepository<Pokemon, String> {
+    
+}
---
+++ src/main/java/fr/efrei/pokemon_tcg/services/PokemonService.java
@@ -0,0 +1,25 @@
+package fr.efrei.pokemon_tcg.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import fr.efrei.pokemon_tcg.models.Pokemon;
+import fr.efrei.pokemon_tcg.repositories.PokemonRepository;
+
+@Service
+public class PokemonService {
+    
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    public Page<Pokemon> getAll(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+
+        Page<Pokemon> pokemons = pokemonRepository.findAll(pageable);
+
+        return pokemons;
+    }
+}
