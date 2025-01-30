2b87baa42c7aaf7e0e043d2b9b4e6d889af1e5e5
samuel-poinama <samuel.poinama@efrei.net>
2025-01-30 11:56:52 +0100

    see inventory

--- src/main/java/fr/efrei/pokemon_tcg/controllers/InventoryController.java
+++ src/main/java/fr/efrei/pokemon_tcg/controllers/InventoryController.java
@@ -3,15 +3,16 @@ package fr.efrei.pokemon_tcg.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.efrei.pokemon_tcg.Dto.CardDto;
 import fr.efrei.pokemon_tcg.Dto.DeckDto;
+import fr.efrei.pokemon_tcg.Dto.InventoryDto;
 import fr.efrei.pokemon_tcg.Dto.TrainerUuidDto;
-import fr.efrei.pokemon_tcg.models.Card;
 import fr.efrei.pokemon_tcg.models.Deck;
 import fr.efrei.pokemon_tcg.models.Inventory;
 import fr.efrei.pokemon_tcg.models.Trainer;
@@ -27,6 +28,21 @@ public class InventoryController {
 
     @Autowired
     private TrainerService trainerService;
+
+
+    @GetMapping("{uuid}")
+    public ResponseEntity<?> getInventory(@PathVariable String uuid) {
+        final Trainer trainer = trainerService.getByUuid(uuid);
+
+        if (trainer == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        final Inventory inventory = trainer.getInventory();
+        
+        final InventoryDto inventoryDto = new InventoryDto(inventory);
+        return new ResponseEntity<>(inventoryDto, HttpStatus.OK);
+    }
     
     @PostMapping("/draw")
     public ResponseEntity<?> drawCard(@RequestBody TrainerUuidDto trainerUuidDto) {
@@ -43,4 +59,5 @@ public class InventoryController {
         final DeckDto deckDto = new DeckDto(deck);
         return new ResponseEntity<>(deckDto, HttpStatus.OK);
     }
+
 }
